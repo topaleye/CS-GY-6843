15899aa9306552cab0de097cd966d34b47a182c5
topaleye <to652@nyu.edu>
2022-09-10 12:26:56 -0400

    correcting 6

--- solution.py
+++ solution.py
@@ -17,7 +17,7 @@ def welcome_assignment_answers(question):
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
         answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = int("5")
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
