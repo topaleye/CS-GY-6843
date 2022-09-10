0dbf77345cd4df5b3498b34c5af9ccf46c56082b
topaleye <to652@nyu.edu>
2022-09-10 12:23:21 -0400

    correcting 1 and 4

--- solution.py
+++ solution.py
@@ -7,13 +7,13 @@ def welcome_assignment_answers(question):
     #The student doesn't have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
         answer = "mtls"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
         answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
